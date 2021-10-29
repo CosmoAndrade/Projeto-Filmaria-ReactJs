4d5b884c35871dd620a05aeddb5f206af0080c0b
CosmoAndrade <cosmo.andrade@hotmail.com>
2021-10-29 15:50:16 -0300

    Adicionando Página de favoritos

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -1,13 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import './favoritos.css'
 
 
 
 function Favoritos(){
+
+    const [filmes, setFilmes] = useState([])
+
+
+    useEffect (() => {
+
+        const minhaLista = localStorage.getItem('filmes')
+        setFilmes(JSON.parse(minhaLista) || [])
+
+    },[])
+
+    function handleDelete(id){
+        let filtroFilmes = filmes.filter((item) => {
+            return(item.id !== id );
+        }) 
+        setFilmes(filtroFilmes)
+        localStorage.setItem('filmes',JSON.stringify(filtroFilmes))
+        
+    }
+
     return(
-        <div>
-            
+        <div id="meus-filmes">
+            <h1>Meu Filmes</h1>
+            <ul>
+                {filmes.map((item) => {
+                    return(
+                        <li key={item.id}>
+                            <span>{item.nome}</span>
+
+                            <div>
+                                <Link to={`/filme/${item.id}`}>Ver Detalhes</Link>
+                        <button onClick={ () => handleDelete(item.id)} > Excluir</button>
+                            </div>
+                        </li>
+                    )
+                })}
+            </ul>
+
         </div>
     )
 }
+
+
 
 export default Favoritos;
